dc3492054034df49c30358983c6aec3087ee9531
Ben Hodes <bhodes@myseneca.ca>
2026-06-03 17:41:42 -0400

    Added Lab4b

---
+++ lab4b.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+def join_lists(l1, l2):
+    # join_lists will return a list that contains every value from both l1 and l2
+    set1 = set(l1)
+    set2 = set(l2)
+    JoinSet = (set1 | set2)
+    return(list(JoinSet))
+
+def match_lists(l1, l2):
+    # match_lists will return a list that contains all values found in both l1 and l2
+    set1 = set(l1)
+    set2 = set(l2)
+    MatchSet = (set1 & set2)
+    return(list(MatchSet))
+
+def diff_lists(l1, l2):
+    # diff_lists will return a list that contains all different values, which are not shared between the lists
+    set1 = set(l1)
+    set2 = set(l2)
+    DiffSet = (set1 ^ set2)
+    return(list(DiffSet))
+
+if __name__ == '__main__':
+    list1 = list(range(1,10))
+    list2 = list(range(5,15))
+    print('list1: ', list1)
+    print('list2: ', list2)
+    print('join: ', join_lists(list1, list2))
+    print('match: ', match_lists(list1, list2))
+    print('diff: ', diff_lists(list1, list2))
